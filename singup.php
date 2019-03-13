abbefb28be7070c8729804fa3163d8ed463dc6b5
amin malekzadeh <aminmalekzadeh004@gmail.com>
2019-03-13 19:49:34 +0330

    Update singup.php

--- singup.php
+++ singup.php
@@ -14,44 +14,85 @@
             margin: 0;
         }
     </style>
-
-    <title>Login</title>
+    <title>singup</title>
 </head>
 <body>
+<?php
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$username1 = $_POST['username'];
+$password1 = $_POST['password'];
+$passwordconfirm = $_POST['confirmpassword'];
+include 'functions.php';
+require_once 'Panel/config.php';
+
+try {
+    $dbPDO = new PDO("mysql:host=$host;dbname=$namedb", $username, $password);
+    if ($_SERVER["REQUEST_METHOD"] == "post") {
+        if ($password1 == $passwordconfirm) {
+            getHash($password1);
+            $Result = $dbPDO->prepare("INSERT INTO users(FullName, Email, Password, username) VALUES (:FullName,:Email,:Password,:username)");
+            $Result->bindParam(':Fullname', $fullname);
+            $Result->bindParam(':Email', $email);
+            $Result->bindParam(':Password', $password1);
+            $Result->bindParam(':username', $username1);
+            $Result->execute();
+            e("اطلاعات شما با موفقیت ثبت شد", "alert-success");
+        } else {
+            e("رمز عبور و تکرار رمزعبور باهم برابر نمی باشد.", "alert-danger");
+        }
+    }
+} catch (PDOException $e) {
+//    e($e, "alert-danger");
+    echo $e;
+}
+
+
+?>
+
 <br>
 <center>
     <div class="container">
         <h1>ثبت نام</h1>
-        <div  class="card w-50 text-dark border-1  rounded">
+        <div class="card w-50 text-dark border-1  rounded">
             <div class="card-body">
 
-                <form action="session_login.php" method="post">
+                <form action="singup.php" method="post">
                     <div class="form-group text-right">
                         <label for="exampleInputEmail1">نام کامل </label>
-                        <input type="text" placeholder="نام کامل" class="form-control text-right" name="fullname" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                        <input type="text" placeholder="نام کامل" class="form-control text-right"
+                               name="fullname"/>
 
                     </div>
                     <div class="form-group text-right">
                         <label for="exampleInputEmail1">ایمیل </label>
-                        <input type="text" placeholder="ایمیل" class="form-control text-right" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                        <input type="text" placeholder="ایمیل" class="form-control text-right"
+                               name="email"/>
 
                     </div>
                     <div class="form-group text-right">
                         <label for="exampleInputEmail1">نام کاربری </label>
-                        <input type="text" placeholder="نام کاربری" class="form-control text-right" name="username" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                        <input type="text" placeholder="نام کاربری" class="form-control text-right"
+                               name="username" id="exampleInputEmail1" required>
 
                     </div>
                     <div class="form-group text-right">
                         <label for="exampleInputPassword1">رمز عبور </label>
-                        <input type="password" placeholder="رمزعبور" class="form-control text-right " name="password" id="exampleInputPassword1">
+                        <input type="password" placeholder="رمزعبور"
+                               class="form-control text-right " name="password"
+                               id="exampleInputPassword1" required>
                     </div>
                     <div class="form-group text-right">
                         <label for="exampleInputPassword1">تکرار رمزعبور</label>
-                        <input type="password" placeholder="تکرار رمزعبور" class="form-control text-right " name="confirmpassword" id="exampleInputPassword1">
+                        <input type="password" placeholder="تکرار رمزعبور"
+                               class="form-control text-right " name="confirmpassword"
+                               id="exampleInputPassword1" required>
                     </div>
                     <a class="btn-link float-right" href="/">بازگشت به صفحه اصلی</a><br/>
 
-                    <center><button type="submit" class="btn btn-primary">ثبت نام</button></center>
+                    <center>
+                        <button type="submit" class="btn btn-primary">ثبت نام</button>
+                    </center>
                 </form>
             </div>
         </div>
